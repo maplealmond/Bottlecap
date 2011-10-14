4e169cb300ae17f5d8bfb299c57fdab401ac1aaf
Michael Amygdalidis <maplealmond@gmail.com>
2011-10-14 00:27:12 -0700

    Database cleaner

--- features/support/test.rb
+++ features/support/test.rb
@@ -3,3 +3,8 @@ require 'mongoid'
 Mongoid.configure do |config|
   config.master = Mongo::Connection.new.db("test")
 end
+
+require 'database_cleaner'
+require 'database_cleaner/cucumber'
+DatabaseCleaner.strategy = :truncation
+DatabaseCleaner.clean
